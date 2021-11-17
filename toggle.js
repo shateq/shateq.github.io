c1bc9d3c870a34a0c0057cdefa979bf0431f3503
Szymon <shateq21@gmail.com>
2021-11-17 18:21:23 +0100

    404 page, new SVG

--- toggle.js
+++ toggle.js
@@ -1,4 +1,3 @@
-// Simple dark mode toggle script! Use with pair of style.css.
 document.addEventListener("DOMContentLoaded", () => {
     let div = document.createElement("div");
     let p = document.createElement("p");
