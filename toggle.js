37e29c76434510ef40f5b7b38bf18bb4aa42cb6a
Szymon <shateq21@gmail.com>
2021-12-23 11:13:42 +0100

    Small things

--- toggle.js
+++ toggle.js
@@ -1,17 +1,15 @@
-// document.addEventListener("DOMContentLoaded", () => {
-    let p = document.createElement("p");
-    p.id = "switch";
-    document.body.appendChild(document.createElement("div").appendChild(p));
+let p = document.createElement("p");
+p.id = "switch";
+document.body.appendChild(document.createElement("div").appendChild(p));
 
-    if (localStorage.getItem("dark") == true.toString()) {
-        document.body.classList.toggle("dark");
-        p.classList.toggle("sun");
-    }
+if (localStorage.getItem("dark") == true.toString()) {
+    document.body.classList.toggle("dark");
+    p.classList.toggle("sun");
+}
 
-    p.onclick = () => {
-        document.body.classList.toggle("dark");
-        p.classList.toggle("sun");
-        let bool = document.body.classList.contains("dark");
-        localStorage.setItem("dark", bool.toString());
-    }
-// }, false);
+p.onclick = () => {
+    document.body.classList.toggle("dark");
+    p.classList.toggle("sun");
+    let bool = document.body.classList.contains("dark");
+    localStorage.setItem("dark", bool.toString());
+}
